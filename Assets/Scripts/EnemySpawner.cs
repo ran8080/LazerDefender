32cfd5a4f7e02ffbadb2735e8305565307b8e28c
ran8080 <ran.gilor@logz.io>
2020-04-22 20:26:18 +0300

    Written a mechanism to send waves of enemies to the screen

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    [SerializeField] int startingWave = 0;
+    [SerializeField] bool looping = false;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        } 
+        while (looping);
+    }
+
+    private IEnumerator SpawnAllWaves() 
+    {
+        for (int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++) { 
+            // Start Couroutine and wait for it to finish
+            yield return StartCoroutine(SpawnAllEnemiesInWave(waveConfigs[waveIndex]));
+        }
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig currentWave)
+    {
+        for (int enemyCount = 0; enemyCount < currentWave.GetNumberOfEnemies(); enemyCount++) 
+        {
+            var newEnemy = Instantiate(
+                currentWave.GetEnemyPrefab(),
+                currentWave.GetWaypoints()[0].transform.position,
+                Quaternion.identity);
+            
+            // This is how we update the wave config for the enemy after we spawn it
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(currentWave);
+            yield return new WaitForSeconds(currentWave.GetTimeBetweenSpawns());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
